92c73d73334c1c30bc78021de8c6a2def817e19d
David BOYER <david.boyer-ISC_France@fr.ibm.com>
2020-01-10 11:01:07 +0100

    test

--- main.tf
+++ main.tf
@@ -23,24 +23,24 @@ terraform {
 
 //--------------------------------------------------------------------
 // Workspace Data
-data "terraform_remote_state" "emea_se_playground_tls_root_certificate" {
-  backend = "remote"
+// data "terraform_remote_state" "emea_se_playground_tls_root_certificate" {
+//   backend = "remote"
 
-  // config = {
-  //   hostname     = "app.terraform.io"
-  //   organization = "emea-se-playground-2019"
-  //   workspaces = {
-  //     name = "tls-root-certificate"
-  //   }
+//   // config = {
+//   //   hostname     = "app.terraform.io"
+//   //   organization = "emea-se-playground-2019"
+//   //   workspaces = {
+//   //     name = "tls-root-certificate"
+//   //   }
 
-  config = {
-    hostname     = "app.terraform.io"
-    organization = "ibm-oss-factory-hackathon"
-    workspaces = {
-      name = "dboyer-hashicorp-full"
-    }
-  } //config
-}
+//   config = {
+//     hostname     = "app.terraform.io"
+//     organization = "ibm-oss-factory-hackathon"
+//     workspaces = {
+//       name = "dboyer-hashicorp-full"
+//     }
+//   } //config
+// }
 
 module "primarycluster" {
   source              = "./modules"
