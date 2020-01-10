907073c725a9673e69aa13d65557114655870550
David BOYER <david.boyer-ISC_France@fr.ibm.com>
2020-01-10 11:21:31 +0100

    update variables

--- variables.tf
+++ variables.tf
@@ -7,22 +7,22 @@
 
 variable "resource_group" {
   description = "The name of your Azure Resource Group."
-  default     = "Azure-Consul-Demo"
+  default     = "dboyer-Azure-Consul-Demo"
 }
 
 variable "demo_prefix" {
   description = "This prefix will be included in the name of some resources."
-  default     = "demostack"
+  default     = "dboyer-demostack"
 }
 
 variable "hostname" {
   description = "VM hostname. Used for local hostname, DNS, and storage-related names."
-  default     = "demostack"
+  default     = "dboyer-demostack"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "westeurope"
 }
 
 
@@ -88,7 +88,7 @@ variable "admin_username" {
 
 variable "admin_password" {
   description = "Administrator password"
-  default     = "replace-with-your-password"
+  default     = "dboyer"
 }
 
 variable "servers" {
